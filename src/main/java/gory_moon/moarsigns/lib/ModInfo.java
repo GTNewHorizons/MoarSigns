5d1db15f18aed8d068900df879b3813dcaaa6dee
bombcar <github@bombcar.com>
2021-12-28 17:57:56 -0600

    new build

--- src/main/java/gory_moon/moarsigns/lib/ModInfo.java
+++ src/main/java/gory_moon/moarsigns/lib/ModInfo.java
@@ -3,7 +3,7 @@ package gory_moon.moarsigns.lib;
 public class ModInfo {
     public static final String ID = "MoarSigns";
     public static final String NAME = "MoarSigns";
-    public static final String VERSION = "@MOD_VERSION@";
+    public static final String VERSION = "GRADLETOKEN_VERSION";
     public static final String FINGERPRINT = "@FINGERPRINT@";
     public static final String CHANNEL = ID.toLowerCase();
     public static final String COMMON_PROXY = "gory_moon.moarsigns.proxy.CommonProxy";
